64e37030084f6ccb7be8030d5b26a4aba0096843
LucasRodriguesSilva <lucasrodrigues19021998@gmail.com>
2022-01-07 15:35:20 -0300

    Criando Array

--- Arrays/Program.cs
+++ Arrays/Program.cs
@@ -6,7 +6,21 @@ namespace Arrays
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int[] arrayInteiros = new int [3];
+            arrayInteiros[0] = 10;
+            arrayInteiros[1] = 20;
+            arrayInteiros[2] = 30;
+
+            System.Console.WriteLine("Primeiro");
+            for (int i = 0; i < arrayInteiros.Length; i++)
+            {
+                System.Console.WriteLine(arrayInteiros[i]);
+            }
+            System.Console.WriteLine("Segundo");
+            foreach (int item in arrayInteiros)
+            {
+                System.Console.WriteLine(item);
+            }
         }
     }
 }
