c6fd7701cdf4896f9c0a55635b4b18d2f9db12c8
LucasRodriguesSilva <lucasrodrigues19021998@gmail.com>
2022-01-09 11:08:22 -0300

    Convertendo uma string para int

--- Arrays/Program.cs
+++ Arrays/Program.cs
@@ -9,18 +9,22 @@ namespace Arrays
             int[] arrayInteiros = new int [3];
             arrayInteiros[0] = 10;
             arrayInteiros[1] = 20;
-            arrayInteiros[2] = 30;
+            // Convertendo uma string para inteiro
+            arrayInteiros[2] = int.Parse("30");
 
+            System.Console.WriteLine("");
             System.Console.WriteLine("Primeiro");
             for (int i = 0; i < arrayInteiros.Length; i++)
             {
                 System.Console.WriteLine(arrayInteiros[i]);
             }
+            System.Console.WriteLine("");
             System.Console.WriteLine("Segundo");
             foreach (int item in arrayInteiros)
             {
                 System.Console.WriteLine(item);
             }
+            System.Console.WriteLine("");
         }
     }
 }
