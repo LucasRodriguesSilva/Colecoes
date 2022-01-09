9b0e350fbc04d155d2f7a08c7fbc0ecd2214678d
LucasRodriguesSilva <lucasrodrigues19021998@gmail.com>
2022-01-09 11:35:20 -0300

    Criando array multidimensional

---
+++ ArrayMultidimensional/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace ArrayMultidimensional
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] matriz = new int[4, 2]
+            {
+                // {} é linhas e dentro é coluna
+                {1, 2},
+                {3, 4},
+                {5, 6},
+                {7, 8}
+            };
+            for (int i = 0; i < matriz.GetLength(0); i++)
+            {
+                for (int j = 0; j < matriz.GetLength(1); j++)
+                {
+                    System.Console.WriteLine(matriz[i, j]);
+                }
+            }
+        }
+    }
+}
